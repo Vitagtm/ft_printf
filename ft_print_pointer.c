f3a4143bb6cad8276ff0122b50b02af511aa27ad
Vitagtm <108814521+Vitagtm@users.noreply.github.com>
2024-11-11 15:19:16 +0100

    Create ft_print_pointer.c
    puntero

---
+++ ft_print_pointer.c
@@ -0,0 +1,17 @@
+#include "ft_printf.h"
+
+int ft_print_pointer(unsigned long long ptr, char *base, int f)
+{
+    int i;
+    i = 0;
+    if(f == 1)
+    {
+        i += write(1, "0x", 2);
+        f = 0;
+    }
+    
+    while(ptr > 16)
+        i += ft_print_pointer(ptr / 16, base, f);
+    write(1, &base[ptr % 16], 1);
+    return i;
+}
