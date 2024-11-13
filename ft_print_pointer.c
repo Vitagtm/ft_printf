82eccbb63b2d5cb47a4340dc7d0199ea66bd123a
Vitagtm <108814521+Vitagtm@users.noreply.github.com>
2024-11-13 15:06:13 +0100

    Update ft_print_pointer.c
    Puntero arreglado

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -4,14 +4,19 @@ int ft_print_pointer(unsigned long long ptr, char *base, int f)
 {
     int i;
     i = 0;
+    if(!ptr)
+    {
+        i += ft_print_str("(nil)");
+        return (i);
+    }
     if(f == 1)
     {
         i += write(1, "0x", 2);
         f = 0;
     }
     
-    while(ptr > 16)
+    while(ptr >= 16)
         i += ft_print_pointer(ptr / 16, base, f);
-    write(1, &base[ptr % 16], 1);
+    i += write(1, &base[ptr % 16], 1);
     return i;
 }
